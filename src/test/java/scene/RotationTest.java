63bea6b7f6bc80eb5163a1b79adbf2938dfcd957
sdrouard <seb.drouard@gmail.com>
2014-04-13 02:20:28 +0200

    Add a test for rotation of planets Move Test3D to correct package

--- src/test/java/scene/RotationTest.java
+++ src/test/java/scene/RotationTest.java
@@ -1,23 +1,30 @@
-import java.awt.Frame;
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
+package scene;
+
+import com.jogamp.opengl.util.FPSAnimator;
+import core.Camera;
+import core.Planet;
+import navigation.Position;
+
 import javax.media.opengl.*;
 import javax.media.opengl.awt.GLCanvas;
-import com.jogamp.opengl.util.*;
+import javax.media.opengl.glu.GLU;
+import java.awt.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 
-public class Test3D implements GLEventListener {
+public class RotationTest implements GLEventListener{
 
-    private double theta = 0;
-    private double s = 0;
-    private double c = 0;
+    ShapeBuilder builder;
+    GLU glu;
+    GL2 gl;
 
     public static void main(String[] args) {
         GLProfile glp = GLProfile.getDefault();
         GLCapabilities caps = new GLCapabilities(glp);
         GLCanvas canvas = new GLCanvas(caps);
 
-        Frame frame = new Frame("AWT Window Test");
-        frame.setSize(300, 300);
+        Frame frame = new Frame("Rotation test");
+        frame.setSize(500, 500);
         frame.add(canvas);
         frame.setVisible(true);
 
@@ -27,7 +34,7 @@ public class Test3D implements GLEventListener {
             }
         });
 
-        canvas.addGLEventListener(new Test3D());
+        canvas.addGLEventListener(new RotationTest());
 
         FPSAnimator animator = new FPSAnimator(canvas, 60);
         animator.add(canvas);
@@ -46,31 +53,29 @@ public class Test3D implements GLEventListener {
 
     @Override
     public void init(GLAutoDrawable drawable) {
+        gl = drawable.getGL().getGL2();
+        glu = new GLU();
+        new Camera(gl, glu, 20);
     }
 
     @Override
     public void reshape(GLAutoDrawable drawable, int x, int y, int w, int h) {
     }
 
-    private void update() {
-        theta += 0.01;
-        s = Math.sin(theta);
-        c = Math.cos(theta);
+    // To override
+    public void update() {
+        //gl.glRotated(2.0,2.0,2.0,2.0);
     }
 
-    private void render(GLAutoDrawable drawable) {
-        GL2 gl = drawable.getGL().getGL2();
+    // To override
+    public void render(GLAutoDrawable drawable) {
+        builder = new ShapeBuilder(glu, gl);
 
         gl.glClear(GL.GL_COLOR_BUFFER_BIT);
 
-        // draw a triangle filling the window
-        gl.glBegin(GL.GL_TRIANGLES);
-        gl.glColor3f(1, 0, 0);
-        gl.glVertex2d(-c, -c);
-        gl.glColor3f(0, 1, 0);
-        gl.glVertex2d(0, c);
-        gl.glColor3f(0, 0, 1);
-        gl.glVertex2d(s, -s);
-        gl.glEnd();
+        builder.newPlanet(new Planet(2.0f, Position.POSITION_000));
+        gl.glRotated(2.0,5.0,2.0,0.0);
+        //gl.glRotated(0.0,2.0,1.0,2.0);
+
     }
 }
